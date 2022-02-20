17c66aed11f4b35588256344681a502c865aef81
Geetika Tirumalasetty <geetikat1702@gmail.com>
2022-02-20 20:51:09 +0530

    fashion website

---
+++ index.js
@@ -0,0 +1,18 @@
+
+$( ".navbar-toggler" ).on("click",function() {
+    $(".navbar-collapse").toggle( );
+  });
+   
+  $(".dt1").hover(function(){
+    $(".menu1").toggle();
+  });
+
+  $(".dt2").hover(function(){
+    $(".menu2").toggle();
+  });
+  $(".dt3").hover(function(){
+    $(".menu3").toggle();
+  });
+  $(".dt4").hover(function(){
+    $(".menu4").toggle();
+  });
